348371a8263e56bef75f12aed9440d726c3e4c99
Christian Donoso <cedonoso@espe.edu.ec>
2026-05-15 16:38:24 -0500

    second commint

--- src/main/java/edu/espe/springprueba/web/controller/DeviceController.java
+++ src/main/java/edu/espe/springprueba/web/controller/DeviceController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/christiandonoso/devices")
 public class DeviceController {
-
+    /*Prueba de CI 1*/
     private final DeviceService service;
 
     public DeviceController(DeviceService service) {
